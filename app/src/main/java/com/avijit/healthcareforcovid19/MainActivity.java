7abbf46371591a8e946108c7dbfe8f54d0e5b2aa
Avijit Acharjee <avijitach@gmail.com>
2020-07-27 12:57:09 +0600

    register

--- app/src/main/java/com/avijit/healthcareforcovid19/MainActivity.java
+++ app/src/main/java/com/avijit/healthcareforcovid19/MainActivity.java
@@ -2,13 +2,24 @@ package com.avijit.healthcareforcovid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView signUpIntentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signUpIntentButton = findViewById(R.id.signup_intent_button);
+        signUpIntentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpUi.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/avijit/healthcareforcovid19/SignUpUi.java
@@ -0,0 +1,73 @@
+package com.avijit.healthcareforcovid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SignUpUi extends AppCompatActivity {
+    EditText nameEditText,emailEditText,phoneEditText,passwordEditText,confirmPasswordEditText;
+    TextView goButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_ui);
+        nameEditText = findViewById(R.id.full_name_edit_text);
+        emailEditText = findViewById(R.id.email_edit_text);
+        phoneEditText = findViewById(R.id.phone_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        goButton = findViewById(R.id.go);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RequestQueue requestQueue = Volley.newRequestQueue(SignUpUi.this);
+                String url = "https://finalproject.xyz/covid_19/api.php";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(SignUpUi.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(SignUpUi.this, "error", Toast.LENGTH_SHORT).show();
+                            }
+                        }){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        return super.getHeaders();
+                    }
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> params = new HashMap<>();
+                        params.put("register","1");
+                        params.put("name",nameEditText.getText().toString());
+                        params.put("email",emailEditText.getText().toString());
+                        params.put("phone",phoneEditText.getText().toString());
+                        params.put("password",passwordEditText.getText().toString());
+                        params.put("user_type_id","1");
+                        return params;
+                    }
+                };
+                requestQueue.add(stringRequest);
+            }
+        });
+    }
+}
