b188809affd1f8ab0aec8be6856c7fd6fcc56cf1
Avijit Acharjee <avijitach@gmail.com>
2020-08-18 04:06:58 +0600

    profile

--- app/src/main/java/com/avijit/healthcareforcovid19/CovidTest.java
+++ app/src/main/java/com/avijit/healthcareforcovid19/CovidTest.java
@@ -3,10 +3,13 @@ package com.avijit.healthcareforcovid19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.avijit.healthcareforcovid19.questionfragments.Q1Fragment;
 import com.avijit.healthcareforcovid19.questionfragments.Q2Fragment;
+import com.avijit.healthcareforcovid19.questionfragments.Q3Fragment;
+import com.avijit.healthcareforcovid19.questionfragments.Q4Fragment;
 
 public class CovidTest extends AppCompatActivity {
     TextView previousButton,nextButton;
@@ -21,23 +24,79 @@ public class CovidTest extends AppCompatActivity {
                 .commit();
         previousButton = findViewById(R.id.prev_button);
         nextButton = findViewById(R.id.next_button);
-
+        previousButton.setVisibility(View.GONE);
         nextButton.setOnClickListener(v -> {
-            if(currentFragment<4){
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.fragment_container,new Q2Fragment())
-                        .commit();
-                currentFragment+=1;
+            switch (currentFragment){
+                case 1: {
+                        getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container,new Q2Fragment())
+                            .commit();
+                    currentFragment+=1;
+                    previousButton.setVisibility(View.VISIBLE);
+                    break;
+                }
+                case 2: {
+                        getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container,new Q3Fragment())
+                            .commit();
+                    currentFragment+=1;
+                    break;
+                }
+                case 3: {
+                        getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container,new Q4Fragment())
+                            .commit();
+                    currentFragment+=1;
+                    nextButton.setVisibility(View.GONE);
+                    break;
+                }
+                case 4: {
+
+                        break;
+                }
+
             }
         });
         previousButton.setOnClickListener(v -> {
-            if(currentFragment>0){
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.fragment_container,new Q1Fragment())
-                        .commit();
-                currentFragment-=1;
+            switch (currentFragment){
+                case 1 :
+                {
+
+                    break;
+                }
+                case 2 :
+                {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container,new Q1Fragment())
+                            .commit();
+                    currentFragment-=1;
+                    previousButton.setVisibility(View.GONE);
+                    break;
+                }
+                case 3 :
+                {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container,new Q2Fragment())
+                            .commit();
+                    currentFragment-=1;
+                    break;
+                }
+                case 4 :
+                {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container,new Q3Fragment())
+                            .commit();
+                    currentFragment-=1;
+                    nextButton.setVisibility(View.VISIBLE);
+                    break;
+                }
+
             }
         });
 
--- app/src/main/java/com/avijit/healthcareforcovid19/Index.java
+++ app/src/main/java/com/avijit/healthcareforcovid19/Index.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class Index extends AppCompatActivity {
-    TextView covidTestIntent;
+    TextView covidTestIntent,profileIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
         covidTestIntent = findViewById(R.id.covid_test_intent);
+        profileIntent = findViewById(R.id.profile);
         covidTestIntent.setOnClickListener(v->{
             startActivity(new Intent(getApplicationContext(),CovidTest.class));
         });
+        profileIntent.setOnClickListener(v->{
+            startActivity(new Intent(getApplicationContext(),Profile.class));
+        });
+
     }
 }
