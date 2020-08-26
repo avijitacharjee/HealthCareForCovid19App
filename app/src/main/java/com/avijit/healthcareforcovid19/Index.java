cca51ffc45a0503820cdb404d0c75a46ef0a73aa
Avijit Acharjee <avijitach@gmail.com>
2020-08-27 01:28:17 +0600

    family info with floating action button

--- app/src/main/java/com/avijit/healthcareforcovid19/Index.java
+++ app/src/main/java/com/avijit/healthcareforcovid19/Index.java
@@ -7,19 +7,27 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class Index extends AppCompatActivity {
-    TextView covidTestIntent,profileIntent;
+    TextView covidTestIntent,profileIntent,familyIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
         covidTestIntent = findViewById(R.id.covid_test_intent);
         profileIntent = findViewById(R.id.profile);
+        familyIntent = findViewById(R.id.family_info);
         covidTestIntent.setOnClickListener(v->{
-            startActivity(new Intent(getApplicationContext(),CovidTest.class));
+            startActivity(CovidTest.class);
         });
         profileIntent.setOnClickListener(v->{
-            startActivity(new Intent(getApplicationContext(),Profile.class));
+            startActivity(Profile.class);
         });
+        familyIntent.setOnClickListener(v->{
+            startActivity(FamilyInfo.class);
+        });
+    }
 
+    public void startActivity(Class cls) {
+        Intent intent=new Intent(getApplicationContext(), cls);
+        super.startActivity(intent);
     }
 }
