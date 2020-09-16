04c625189828ae6e308b48674e13ffb5c5e4c45e
Avijit Acharjee <avijitach@gmail.com>
2020-09-17 03:23:39 +0600

    r fragment

--- app/src/main/java/com/avijit/healthcareforcovid19/CovidTest.java
+++ app/src/main/java/com/avijit/healthcareforcovid19/CovidTest.java
@@ -11,46 +11,48 @@ import com.avijit.healthcareforcovid19.questionfragments.Q1Fragment;
 import com.avijit.healthcareforcovid19.questionfragments.Q2Fragment;
 import com.avijit.healthcareforcovid19.questionfragments.Q3Fragment;
 import com.avijit.healthcareforcovid19.questionfragments.Q4Fragment;
+import com.avijit.healthcareforcovid19.questionfragments.RFragment;
 
 public class CovidTest extends AppCompatActivity {
-    TextView previousButton,nextButton;
-    int currentFragment=1;
+    TextView previousButton, nextButton;
+    int currentFragment = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_covid_test);
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.fragment_container,new Q1Fragment())
+                .add(R.id.fragment_container, new Q1Fragment())
                 .commit();
         previousButton = findViewById(R.id.prev_button);
         nextButton = findViewById(R.id.next_button);
         previousButton.setVisibility(View.GONE);
         nextButton.setOnClickListener(v -> {
-            switch (currentFragment){
+            switch (currentFragment) {
                 case 1: {
-                        getSupportFragmentManager()
+                    getSupportFragmentManager()
                             .beginTransaction()
-                            .replace(R.id.fragment_container,new Q2Fragment())
+                            .replace(R.id.fragment_container, new Q2Fragment())
                             .commit();
-                    currentFragment+=1;
+                    currentFragment += 1;
                     previousButton.setVisibility(View.VISIBLE);
                     break;
                 }
                 case 2: {
-                        getSupportFragmentManager()
+                    getSupportFragmentManager()
                             .beginTransaction()
-                            .replace(R.id.fragment_container,new Q3Fragment())
+                            .replace(R.id.fragment_container, new Q3Fragment())
                             .commit();
-                    currentFragment+=1;
+                    currentFragment += 1;
                     break;
                 }
                 case 3: {
-                        getSupportFragmentManager()
+                    getSupportFragmentManager()
                             .beginTransaction()
-                            .replace(R.id.fragment_container,new Q4Fragment())
+                            .replace(R.id.fragment_container, new Q4Fragment())
                             .commit();
-                    currentFragment+=1;
+                    currentFragment += 1;
                     nextButton.setText(R.string.submit);
                     nextButton.setBackgroundColor(Color.parseColor("#4CAF50"));
                     nextButton.setTextColor(Color.parseColor("#FFFFFF"));
@@ -58,46 +60,57 @@ public class CovidTest extends AppCompatActivity {
                     break;
                 }
                 case 4: {
-
-                        break;
+                    previousButton.setVisibility(View.GONE);
+                    nextButton.setText("Done");
+                    nextButton.setBackgroundColor(Color.parseColor("#ab47bc"));
+                    currentFragment += 1;
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new RFragment())
+                            .commit();
+                    break;
+                }
+                case 5: {
+                    onBackPressed();
+                    break;
                 }
 
             }
         });
         previousButton.setOnClickListener(v -> {
-            switch (currentFragment){
-                case 1 :
-                {
+            switch (currentFragment) {
+                case 1: {
 
                     break;
                 }
-                case 2 :
-                {
+                case 2: {
                     getSupportFragmentManager()
                             .beginTransaction()
-                            .replace(R.id.fragment_container,new Q1Fragment())
+                            .replace(R.id.fragment_container, new Q1Fragment())
                             .commit();
-                    currentFragment-=1;
+                    currentFragment -= 1;
                     previousButton.setVisibility(View.GONE);
                     break;
                 }
-                case 3 :
-                {
+                case 3: {
                     getSupportFragmentManager()
                             .beginTransaction()
-                            .replace(R.id.fragment_container,new Q2Fragment())
+                            .replace(R.id.fragment_container, new Q2Fragment())
                             .commit();
-                    currentFragment-=1;
+                    currentFragment -= 1;
                     break;
                 }
-                case 4 :
-                {
+                case 4: {
                     getSupportFragmentManager()
                             .beginTransaction()
-                            .replace(R.id.fragment_container,new Q3Fragment())
+                            .replace(R.id.fragment_container, new Q3Fragment())
                             .commit();
-                    currentFragment-=1;
+                    currentFragment -= 1;
                     nextButton.setVisibility(View.VISIBLE);
+
+                    nextButton.setText(R.string.next);
+                    nextButton.setBackgroundColor(getResources().getColor(R.color.white_1));
+                    nextButton.setTextColor(getResources().getColor(R.color.black_1));
                     break;
                 }
 
--- app/src/main/java/com/avijit/healthcareforcovid19/questionfragments/RFragment.java
+++ app/src/main/java/com/avijit/healthcareforcovid19/questionfragments/RFragment.java
@@ -1,8 +1,24 @@
 package com.avijit.healthcareforcovid19.questionfragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.avijit.healthcareforcovid19.R;
+
 /**
  * Created by Avijit Acharjee on 9/17/2020 at 2:52 AM.
  * Email: avijitach@gmail.com.
  */
-class RFragment {
+public class RFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_r,null,false);
+    }
 }
